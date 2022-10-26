83800037ad6543021227aa579253bfe804c01e6c
devcom2020 <kwp@tlen.pl>
2022-10-26 13:45:19 +0200

    Initial commit

---
+++ pierwszy.py
@@ -0,0 +1,18 @@
+# print('mam na imię Tomek')
+# print("mam na imię Tomek")
+
+wiek = 39  # to jest zmienna typu int
+# print(id(wiek))
+
+wiek2 = 38
+# print(id(wiek2))
+
+adresStrony = "www"
+adres_strony = "www."
+# tutaj będę wykonywał dalszą cześć programu
+# print(wiek)
+
+# print(id(adresStrony))
+# print(id(adres_strony))
+
+
---
+++ typy_danych.py
@@ -0,0 +1,44 @@
+wiek = 39
+rok = 2022
+temp = 36.6
+# print(wiek + rok)
+# print(wiek - rok)
+# print(wiek * rok)
+# print(wiek / rok)
+# print(wiek // rok)
+# print(wiek ** rok)
+# print(wiek % rok)
+# print(54 - (5 * 43) + (4 / 2 + 4) / 2)
+
+# print("\tsprawdzam zmienną 🥸 temp: {} \n".format(type(wiek)))
+# print(f"sprawdzam zmienną wiek:{type(wiek)}🥸")
+#
+# print(f"""              🥸🥸
+#                 sprawdzam zmienną temp:
+#                     {type(temp)}
+#                 sprawdzam zmienną wiek:
+#                     {type(wiek)}
+#                 wynik naszego działania:
+#                 {54 - (5 * 43) + (4 / 2 + 4) / 2}
+#                 🥸🥸              🥸🥸
+# """)
+
+
+imie = "Tomek Tomek"
+imie2 = imie.lower()
+imie3 = imie.upper()
+# print(imie2.count("tomek"))
+# print(imie3)
+tytul = "Tutaj tytuł"
+print(tytul.rstrip("tytuł"))
+print(tytul.lstrip())
+print(tytul[0:7])
+print(tytul[:5])
+print(tytul[:-1])
+print(tytul[-5:])
+czy_znasz_Python = True
+wartosc = int(czy_znasz_Python)
+# print(type(czy_znasz_Python))
+# print(type(wartosc))
+# print(wartosc)
+
