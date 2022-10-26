213e331d11b6d5248ffcb511bdaf0ff1566fde02
devcom2020 <kwp@tlen.pl>
2022-10-26 15:36:47 +0200

    środa 15:36

---
+++ typy_zl4.py
@@ -0,0 +1,13 @@
+slownik = {}
+slownik["imie"] = "Tomek"
+slownik["imie"] = "Andrzej"
+slownik["wiek"] = 39
+lista = [44, 55, 66, 77, 33, 22, 11, 55, 33, 11]
+slownik["liczby"] = lista
+print(slownik)
+# print(slownik.keys())
+# print(slownik.values())
+# print(slownik.items())
+print(slownik['liczby'][0])
+
+
