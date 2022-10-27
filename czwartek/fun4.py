4ef6410699ca2178251c101deeb03ed93a0f4797
devcom2020 <kwp@tlen.pl>
2022-10-27 15:59:40 +0200

    czwartek 16:00

--- czwartek/fun4.py
+++ czwartek/fun4.py
@@ -14,8 +14,10 @@ def connect(**opcje):
     print(conn_param)
     # print(opcje)
 
+
 connect()
 connect(root='/')
+
 
 # liczby(5, 4, 43, 7)
 # liczby(7)
@@ -25,3 +27,26 @@ connect(root='/')
 # krotka = 4,
 # print(krotka)
 
+def wypisz_1(a, b, c):
+    print(f"a={a} b={b} c={c}")
+
+
+def wypisz(a, b, /, c):
+    print(f"a={a} b={b} c={c}")
+
+
+wypisz_1(c=1, a=2, b=3)
+wypisz(1, 2, 3)
+wypisz(1, 2, c=3)
+
+
+# wypisz(c=1, a=2, b=3)
+
+def allparams(a, b, /, c=42, *args, d=256, e, **kwargs):
+    print('a, b, c:', a, b, c)
+    print('d, e:', d, e)
+    print('args:', args)
+    print('kwargs:', kwargs)
+
+
+allparams(2, 6, e=10, f=11)
