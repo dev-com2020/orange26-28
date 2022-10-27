8839963986832b701b48e56b1d837cbb60330cba
devcom2020 <kwp@tlen.pl>
2022-10-27 07:20:06 +0200

    czwartek 7:20

--- sroda/typy_zl4.py
+++ sroda/typy_zl4.py
@@ -3,11 +3,12 @@ slownik["imie"] = "Tomek"
 slownik["imie"] = "Andrzej"
 slownik["wiek"] = 39
 lista = [44, 55, 66, 77, 33, 22, 11, 55, 33, 11]
+lista2 = [44, 55, 66, 11]
 slownik["liczby"] = lista
 print(slownik)
 # print(slownik.keys())
 # print(slownik.values())
 # print(slownik.items())
 print(slownik['liczby'][0])
-
-
+nowa = lista + lista2
+print(nowa)
