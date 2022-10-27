849cbc8c44ecdc7ebd6e84ff1904d689699e6970
devcom2020 <kwp@tlen.pl>
2022-10-27 13:48:24 +0200

    czwartek 13:48

--- czwartek/triki39.py
+++ czwartek/triki39.py
@@ -1,12 +1,27 @@
-d = {'a': 'A', 'b': 'B'}
-e = {'b': 8, 'c': 'C'}
-s = 'Hello There'
+# d = {'a': 'A', 'b': 'B'}
+# e = {'b': 8, 'c': 'C'}
+# s = 'Hello There'
+#
+# print(d | e)
+# print(e | d)
+# print({**d, **e})
+#
+# print(s.removeprefix("Hello"))
+# print(s.removesuffix("There"))
+# print(s.removeprefix("Python"))
 
-print(d | e)
-print(e | d)
-print({**d, **e})
 
-print(s.removeprefix("Hello"))
-print(s.removesuffix("There"))
-print(s.removeprefix("Python"))
+wybor = input("Napisz swoją ulubioną liczbę: ")
+
+match wybor:
+    case "1":
+        print("Twoja ulubiona cyfra to", wybor)
+    case "3":
+        print("Twoja ulubiona cyfra to", wybor)
+    case "5":
+        print("Twoja ulubiona cyfra to", wybor)
+    case "4":
+        print("Twoja ulubiona cyfra to", wybor)
+    case _:
+        print("**Twoja ulubiona cyfra to**", wybor)
 
