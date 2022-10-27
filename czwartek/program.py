f67c9b489557ee61991b302eb82ca569c0d49602
devcom2020 <kwp@tlen.pl>
2022-10-27 12:57:52 +0200

    czwartek 12:57

---
+++ czwartek/program.py
@@ -0,0 +1,16 @@
+while True:
+    print("""
+        1. dodawanie
+        2. odejmowanie
+        3. koniec
+    """)
+    wybor = input("Wprowadź numer(1-3):")
+    if wybor == "1":
+        print("Dodajemy...")
+    elif wybor == "2":
+        print("odejmujemy...")
+    else:
+        print("Dziękujemy, do widzenia!")
+        break
+input("Aby zakończyć program, wciśnij ENTER")
+
