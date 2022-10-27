bbf1dd3dc1917fb10fb5736603577d33f219e913
devcom2020 <kwp@tlen.pl>
2022-10-27 09:46:22 +0200

    czwartek 9:46

---
+++ czwartek/triki39.py
@@ -0,0 +1,12 @@
+d = {'a': 'A', 'b': 'B'}
+e = {'b': 8, 'c': 'C'}
+s = 'Hello There'
+
+print(d | e)
+print(e | d)
+print({**d, **e})
+
+print(s.removeprefix("Hello"))
+print(s.removesuffix("There"))
+print(s.removeprefix("Python"))
+
