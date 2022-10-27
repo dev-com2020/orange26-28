bdc22adab534607d6e1664cca88fd2fc247135e0
devcom2020 <kwp@tlen.pl>
2022-10-27 15:07:26 +0200

    czwartek 15:07

--- czwartek/fun3.py
+++ czwartek/fun3.py
@@ -16,3 +16,8 @@ print(vat1)
 lista = [1, 3, 5, 7]
 print(f"Zastosowanie map: {list(map(lambda x: x * 2, lista))}")
 print(f"Zastosowanie filter: {list(filter(lambda x: x > 3, lista))}")
+
+wiek = lambda x: "dziecko" if x < 10 else ("Nastolatek" if x < 18 else "Dorosły")
+print(wiek(5))
+print(wiek(15))
+print(wiek(25))
