07cc4df98d6b8298f61e3c59ef20106be615761c
devcom2020 <kwp@tlen.pl>
2022-10-28 14:41:15 +0200

    piatek 14:41

--- main.py
+++ main.py
@@ -10,20 +10,30 @@ from piatek import klasa1 as k1
 
 
 # if __name__ == '__main__':
-    # haslo = input("Podaj hasło: ")
-    #
-    # if not login.wazne_haslo(haslo):
-    #     print("Hasło jest nieprawidłowe")
-    # else:
-    #     print("OK, hasło jest poprawne!")
-
-dom1 = k1.Dom()
-print(dom1)
+# haslo = input("Podaj hasło: ")
+#
+# if not login.wazne_haslo(haslo):
+#     print("Hasło jest nieprawidłowe")
+# else:
+#     print("OK, hasło jest poprawne!")
+#
+# dom1 = k1.Dom()
+# print(dom1)
 # print(dom1.metraz)
 # dom1.okresl_metraz()
 # print(dom1.metraz)
-dom1.okresl_kolor()
+# dom1.okresl_kolor()
 
-dom2 = k1.Dom()
-print(dom2)
-dom2.okresl_kolor()
+# dom2 = k1.Dom()
+# print(dom2)
+# dom2.okresl_kolor()
+# print("Dom2 ma kolor:", dom2.kolor)
+
+# dom3 = k1.Dom2(100, "czerwony", 3)
+# print(dom3.metraz)
+# print(dom3.ilosc_okien)
+# print(dom3.kolor)
+# dom3.zmien_okna()
+# print(dom3.ilosc_okien)
+# dom3.metraz = -100
+# print(dom3.metraz)
--- piatek/klasa1.py
+++ piatek/klasa1.py
@@ -1,4 +1,8 @@
 class Dom:
+    '''
+    To jest klasa DOM, która sprawdza działanie
+    OOP w Pythonie...
+    '''
     # pola klasy
     metraz = 0
     kolor = ""
@@ -15,6 +19,55 @@ class Dom:
         print("Kolor wynosi: ", self.kolor)
 
     def okresl_okna(self):
+        wybor = int(input("Podaj ilość okien "))
+        self.ilosc_okien = wybor
+        print("Ilość okien wynosi: ", self.ilosc_okien)
+
+
+class Dom2:
+    '''
+    To jest klasa DOM2, która sprawdza działanie
+    OOP w Pythonie...
+    '''
+    def __init__(self, metraz, kolor, ilosc_okien):
+        self.metraz = metraz
+        self.kolor = kolor
+        self.ilosc_okien = ilosc_okien
+    def zmien_metraz(self):
+        wybor = int(input("Podaj metraż "))
+        self.metraz = wybor
+        print("Metraż wynosi: ", self.metraz)
+
+    def zmien_kolor(self):
+        wybor = input("Podaj kolor ")
+        self.kolor = wybor
+        print("Kolor wynosi: ", self.kolor)
+
+    def zmien_okna(self):
+        wybor = int(input("Podaj ilość okien "))
+        self.ilosc_okien = wybor
+        print("Ilość okien wynosi: ", self.ilosc_okien)
+
+class Dom3:
+    '''
+    To jest klasa DOM3, która sprawdza działanie
+    OOP w Pythonie...
+    '''
+    def __init__(self, metraz, kolor, ilosc_okien):
+        self.metraz = metraz
+        self.kolor = kolor
+        self.ilosc_okien = ilosc_okien
+    def zmien_metraz(self):
+        wybor = int(input("Podaj metraż "))
+        self.metraz = wybor
+        print("Metraż wynosi: ", self.metraz)
+
+    def zmien_kolor(self):
+        wybor = input("Podaj kolor ")
+        self.kolor = wybor
+        print("Kolor wynosi: ", self.kolor)
+
+    def zmien_okna(self):
         wybor = int(input("Podaj ilość okien "))
         self.ilosc_okien = wybor
         print("Ilość okien wynosi: ", self.ilosc_okien)
