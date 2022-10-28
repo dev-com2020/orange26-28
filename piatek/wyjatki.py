a708a458032d2cf319189af8414f276ab259f8bc
devcom2020 <kwp@tlen.pl>
2022-10-28 10:48:23 +0200

    piatek 10:48

--- piatek/wyjatki.py
+++ piatek/wyjatki.py
@@ -5,17 +5,38 @@
 #         print("błąd typu, zwracam bezpieczne 0")
 #         return 0
 
+# def mnozenie(a, b):
+#     try:
+#         return int(a) * int(b)
+#     except TypeError:
+#         print("błąd typu, zwracam bezpieczne 0")
+#         return 0
+#     except ValueError:
+#         print("błąd wartości, zwracam bezpieczne 0")
+#         return 0
+
+# def mnozenie(a, b):
+#     try:
+#         return int(a) * int(b)
+#     except Exception as e:
+#         print("Wystąpił:", e)
+#         return 0
+
 def mnozenie(a, b):
     try:
-        return int(a) * int(b)
-    except TypeError:
-        print("błąd typu, zwracam bezpieczne 0")
-        return 0
-    except ValueError:
-        print("błąd wartości, zwracam bezpieczne 0")
-        return 0
+        print(int(a) * int(b))
+    except Exception as e:
+        print("Wystąpił:", e)
+        print(0)
+    else:
+        wynik = []
+        wynik.append(a)
+        wynik.append(b)
+        print(wynik)
+    finally:
+        print("Wykonywałem instrukcje z funkcji mnozenie()")
 
 
-print(mnozenie(3, 4))
-print(mnozenie("3", "4"))
-print(mnozenie("a", "b"))
+mnozenie(3, 4)
+mnozenie("3", "4")
+mnozenie("a", "b")
