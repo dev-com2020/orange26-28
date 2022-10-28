7abd52dae18ece136b95ac2c254f0361ba1f574c
devcom2020 <kwp@tlen.pl>
2022-10-28 12:57:10 +0200

    piatek 12:57

---
+++ piatek/csvki.py
@@ -0,0 +1,9 @@
+import csv
+
+fields = ["name", "branch", "year", 'CGPS']
+rows = [
+    ['Sanchit', 'COE', '2', '9.1'],
+    ['Prateek', 'MCE', '3', '7.8'],
+    ['Sagar', 'SE', '1', '9.5'],
+    ['Nikhil', 'COE', '2', '9.0'],
+]
